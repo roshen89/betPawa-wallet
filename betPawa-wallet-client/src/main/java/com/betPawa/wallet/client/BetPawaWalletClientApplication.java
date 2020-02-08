cdf9db11e12eb906141f70ce91925ba2791efd99
ROSH <rovshen.quliyev@voidaint.com>
2020-02-08 18:06:27 +0200

    created CLI

--- betPawa-wallet-client/src/main/java/com/betPawa/wallet/client/BetPawaWalletClientApplication.java
+++ betPawa-wallet-client/src/main/java/com/betPawa/wallet/client/BetPawaWalletClientApplication.java
@@ -1,12 +1,31 @@
 package com.betPawa.wallet.client;
 
+import com.betPawa.wallet.client.dto.WalletClientRequest;
+import com.betPawa.wallet.client.service.WalletClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BetPawaWalletClientApplication {
 
+  @Autowired
+  WalletClientService walletClientService;
+
   public static void main(String[] args) {
     SpringApplication.run(BetPawaWalletClientApplication.class, args);
   }
+
+  @Bean
+  public CommandLineRunner execute() {
+    WalletClientRequest walletClientRequest = WalletClientRequest.builder()
+        .numberOfRequests(1L)
+        .numberOfRounds(1L)
+        .numberOfUsers(10L)
+        .build();
+    return args -> walletClientService.run(walletClientRequest);
+  }
+
 }
--- betPawa-wallet-client/src/main/java/com/betPawa/wallet/client/dto/WalletClientRequest.java
+++ betPawa-wallet-client/src/main/java/com/betPawa/wallet/client/dto/WalletClientRequest.java
@@ -1,10 +1,12 @@
 package com.betPawa.wallet.client.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @AllArgsConstructor
 @Data
+@Builder(toBuilder = true)
 public final class WalletClientRequest {
 
   private Long numberOfUsers;

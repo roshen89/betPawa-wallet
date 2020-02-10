8d7d9f43914bce4045f3fa9629661521d4f9d6f3
ROSH <rovshen.quliyev@voidaint.com>
2020-02-10 16:47:56 +0200

    Unit Test WalletClientController

--- betPawa-wallet-client/src/main/java/com/betPawa/wallet/client/dto/WalletClientRequest.java
+++ betPawa-wallet-client/src/main/java/com/betPawa/wallet/client/dto/WalletClientRequest.java
@@ -3,10 +3,12 @@ package com.betPawa.wallet.client.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@AllArgsConstructor
 @Data
 @Builder(toBuilder = true)
+@AllArgsConstructor
+@NoArgsConstructor
 public final class WalletClientRequest {
 
   private Long countOfUsers;
---
+++ betPawa-wallet-client/src/test/java/com/betPawa/wallet/client/controller/WalletClientControllerTest.java
@@ -0,0 +1,65 @@
+package com.betPawa.wallet.client.controller;
+
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.betPawa.wallet.client.dto.WalletClientRequest;
+import com.betPawa.wallet.client.dto.WalletClientResponse;
+import com.betPawa.wallet.client.service.WalletClientService;
+import com.betPawa.wallet.proto.OPERATION;
+import com.betPawa.wallet.proto.STATUS;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.EnumMap;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+public class WalletClientControllerTest {
+
+  @InjectMocks
+  WalletClientController clientController;
+
+  @Mock
+  WalletClientService clientService;
+
+  private Map<OPERATION, Map<STATUS, AtomicLong>> operationMapMap;
+
+  private WalletClientRequest clientRequest;
+  private WalletClientResponse clientResponse;
+
+  protected MockMvc mockMvc;
+  protected ObjectMapper mapper;
+
+  @Before
+  public void setUp() {
+    this.mockMvc = MockMvcBuilders.standaloneSetup(clientController).build();
+    mapper = new ObjectMapper();
+    clientRequest = WalletClientRequest.builder().countOfRequests(1L).countOfRounds(1L).countOfUsers(100L).build();
+    operationMapMap = new EnumMap<>(OPERATION.class);
+    clientResponse = WalletClientResponse.builder().transactions(operationMapMap).timeTaken(0L).build();
+  }
+
+  @Test
+  public void execute() throws Exception {
+    Mockito.when(clientService.run(clientRequest)).thenReturn(operationMapMap);
+    MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.post("/" )
+        .contentType(MediaType.APPLICATION_JSON)
+        .content(mapper.writeValueAsString(clientRequest)))
+        .andExpect(status().isOk())
+        .andReturn();
+    assertThat(mvcResult.getResponse().getContentAsString()).isEqualTo(mapper.writeValueAsString(clientResponse));
+  }
+}
